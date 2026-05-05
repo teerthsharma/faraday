f7b451ac82098e9fbe1d091920f5263c52409c8b
teerth sharma <teerth.2428010112@muj.manipal.edu>
2026-05-06 04:30:29 +0530

    Create test_eigsh.py

---
+++ test_eigsh.py
@@ -0,0 +1,5 @@
+import numpy as np
+from faraday.em_solver import CavityGeometry, CavityShape, solve_cavity_modes
+geom = CavityGeometry(shape=CavityShape.RECTANGULAR, dims=(2.0, 1.0))
+res = solve_cavity_modes(geom, nx=20, ny=20, num_modes=3)
+print(res["k_values"])
